6cae3b74f01769fb9508994c69750310c0f6859c
samanta2403 <89295518+samanta2403@users.noreply.github.com>
2022-12-18 11:54:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+canvas -
+document.getElementById("myCanvas")  ;
+ctx=canvas.getContext("2d");
+
+ctx.beginPath()
+ctx.strokeStyle="red";
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200* Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath()
+ctx.strokeStyle="blue";
+ctx.lineWidth=5;
+ctx.rect(250,210,40,0,2* Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath()
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.rect(300,210,40,0,2 * Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath()
+ctx.strokeStyle="yellow";
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200* Math.PI);
+ctx.strokeStyle();
+
+ctx.beginPath()
+ctx.strokeStyle="green";
+ctx.lineWidth=1;
+ctx.rect(150,143,430,200* Math.PI);
+ctx.strokeStyle();
